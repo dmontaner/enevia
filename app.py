3c3706bae53a257c2600f27db484c536ce09045f
dmontaner <david.montaner@gmail.com>
2021-06-27 00:22:20 +0100

    GUI

---
+++ app.py
@@ -0,0 +1,67 @@
+# https://github.com/JackFlexington/PDFextract_text/
+
+import PyPDF2
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter.filedialog import askopenfile
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("PDF Extract Text")
+root.resizable(False, False)
+
+N = 2
+
+canvas = tk.Canvas(root, width=600 * N, height=300 * N)
+canvas.grid(columnspan=3, rowspan=3)
+
+# logo
+logo = Image.open('logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+# instructions
+instructions = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Raleway")
+instructions.grid(columnspan=3, column=0, row=1)
+
+
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title="Choose a file")  # , filetype=[("Pdf file", "*.pdf")])
+    if file:
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+
+        # text box
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+
+        browse_text.set("Browse")
+
+
+# function for event
+def on_closing():
+    if messagebox.askyesno("PDF Extract Text", "ಡ ﹏ ಡ\nDo you really want to go...?"):
+        root.destroy()
+
+
+# browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda: open_file(), font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+browse_text.set("Browse")
+browse_btn.grid(column=1, row=2)
+
+canvas = tk.Canvas(root, width=600 * N, height=250 * N)
+canvas.grid(columnspan=3)
+
+# window manager delete window event
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+# main program
+root.mainloop()

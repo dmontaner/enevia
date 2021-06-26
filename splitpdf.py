da6f78131b88435a92f0b20cd4f02fdc90afa095
dmontaner <david.montaner@gmail.com>
2021-06-26 23:52:41 +0100

    with functions

--- splitpdf.py
+++ splitpdf.py
@@ -1,6 +1,6 @@
 # splitpdf.py
-# 2021-06-24 david.montaner@gmail.com
-# split pdf by fix number of pages
+# 2021-06-26 david.montaner@gmail.com
+# split pdf for Enevia
 
 import math
 import argparse
@@ -21,7 +21,6 @@ def read_teletest_info(file):
                 'line_3' : page.lines[3],
                 'ref_lines': [x for x in page.lines if x.startswith('Ref:')]
             })
-
         cuts = {i: (
             str(x['page_no']),
             x['line_3'].replace(': ', '_'),
@@ -33,7 +32,10 @@ def read_teletest_info(file):
     return n_pages, is_teletest, page_data, cuts
 
 
-def create_batch_files(reader, cuts, output_tag):
+def create_batch_files(file, cuts, output_tag):
+    reader = PyPDF2.PdfFileReader(open(file, 'rb'))
+    n_pages = reader.numPages
+
     batch = page = 0
     writer = PyPDF2.PdfFileWriter()
     writer.addPage(reader.getPage(page))
@@ -53,12 +55,45 @@ def create_batch_files(reader, cuts, output_tag):
     writer.write(open(output_file, 'wb'))
 
 
+def cuts_from_comas(cuts):
+    '''
+    cuts: string of coma separated page numbers (1 based).
+    '''
+    cuts = cuts.strip(',').split(',')
+    cuts = [int(x) - 1 for x in cuts]
+    cuts.append(0)
+    cuts = sorted(list(set(cuts)))
+    cuts = {x: (str(x + 1),) for x in cuts}
+    return cuts
+
+
+def cuts_from_batches(batches, n_pages):
+    '''
+    batches: number of batches to be done.
+    n_pages: number of pages in the document.
+    '''
+    step = math.ceil(n_pages / batches)
+    cuts = list(range(0, n_pages, step))
+    cuts = {x: (str(x + 1),) for x in cuts}
+    return cuts
+
+
+def cuts_from_every(every, n_pages):
+    '''
+    every  : number of pages in each batch document.
+    n_pages: number of pages in the document.
+    '''
+    cuts = list(range(0, n_pages, every))
+    cuts = {x: (str(x + 1),) for x in cuts}
+    return cuts
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='''
     Split PDF tool:
     ''')
-    parser.add_argument('-v', '--version', action='version', version='0.0.0')
+    parser.add_argument('-v', '--version', action='version', version='0.0.1')
     # parser.add_argument('-t', '--no-teletest', help='Do not attempt to parse Teletest data', action='store_true')
     parser.add_argument('-c', '--cuts'   , type=str, help='Collection of comma separated page cuts where new batches should start. Ej: 1,5,12')
     parser.add_argument('-b', '--batches', type=int, help='Number batches to be created.')
@@ -76,38 +111,30 @@ if __name__ == '__main__':
 
     print(input_file)
 
-    # read input file
-    reader = PyPDF2.PdfFileReader(open(input_file, 'rb'))
-    n_pages = reader.numPages
+    # PREPROCESS FILE
+    n_pages, is_teletest, _, teletest_cuts = read_teletest_info(input_file)
 
     if n_pages == 0:
         print(f'\nFile `{input_file}` seems to be empty.\n')
         raise Exception
 
     if args.cuts:
-        cuts = args.cuts.strip(',').split(',')
-        cuts = [int(x) - 1 for x in cuts]
-        cuts.append(0)
-        cuts = sorted(list(set(cuts)))
-        cuts = {x: (str(x + 1),) for x in cuts}
+        cuts = cuts_from_comas(args.cuts)
     elif args.batches:
-        step = math.ceil(n_pages / args.batches)
-        cuts = list(range(0, n_pages, step))
-        cuts = {x: (str(x + 1),) for x in cuts}
+        cuts = cuts_from_batches(args.batches, n_pages)
     elif args.every:
-        cuts = list(range(0, n_pages, args.every))
-        cuts = {x: (str(x + 1),) for x in cuts}
+        cuts = cuts_from_every(args.every, n_pages)
+    elif is_teletest and 0 < len(teletest_cuts):
+        print('\nTeletest file detected.\n')
+        cuts = teletest_cuts
     else:
-        _, is_teletest, _, cuts = read_teletest_info(input_file)
-        if is_teletest and 0 < len(cuts):
-            print('\nTeletest data detected.\n')
-        else:
-            print('\nTeletest data not detected.\nOne of this parameters is then required: `--cuts`, `--batches` or `--every`\nSee option -h for HELP`\n')
-            raise Exception
+        print('\nTeletest data not detected.\nOne of this parameters is then required: `--cuts`, `--batches` or `--every`\nSee option -h for HELP`\n')
+        raise Exception
 
     if args.output:
         output_tag = args.output
     else:
         output_tag = input_file[:-4]
 
-    create_batch_files(reader, cuts, output_tag)
+    # PROCESS FILE
+    create_batch_files(input_file, cuts, output_tag)

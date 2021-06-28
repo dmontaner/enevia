40f847b36d9225a9b0ea91c2f04e17bf622ef7f3
dmontaner <david.montaner@gmail.com>
2021-06-28 22:41:44 +0100

    set up the desktop app

---
+++ ubuntu_desktop_app/setup_ubuntu_desktop_app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# setup_ubuntu_desktop_app.sh
+# 2021-06-28 david.montaner@gmali.com
+# install the desktop app in ubuntu
+
+sudo cp enevia_logo.png /usr/share/icons/
+sudo cp enevia.desktop  /usr/share/applications

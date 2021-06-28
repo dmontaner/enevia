c646e41bf9bcfeff24b8d0265378deac45257699
dmontaner <david.montaner@gmail.com>
2021-06-28 23:03:15 +0100

    copying application

--- ubuntu_desktop_app/setup_ubuntu_desktop_app.sh
+++ ubuntu_desktop_app/setup_ubuntu_desktop_app.sh
@@ -3,5 +3,8 @@
 # 2021-06-28 david.montaner@gmali.com
 # install the desktop app in ubuntu
 
+sudo cp ../splitpdf.py /usr/bin/splitpdf
+sudo chmod +x /usr/bin/splitpdf
+
 sudo cp enevia_logo.png /usr/share/icons/
 sudo cp enevia.desktop  /usr/share/applications
